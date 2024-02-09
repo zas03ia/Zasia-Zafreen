fba3b33995b55ebbebdee371cce9cda0a1183cc3
Zasia Zafreen <65683209+zas03ia@users.noreply.github.com>
2024-02-09 19:48:06 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+function expand(id) {
+    var source=`${id}.html`
+  document.getElementById("iframe").src = source;  
+  var popup = document.getElementById('content');
+  popup.style.display = "block";
+  var header = document.getElementsByTagName("header")[0];
+
+  header.style.display = "none";
+  scrollTop =
+            window.pageYOffset ||
+            document.documentElement.scrollTop;
+scrollLeft =
+            window.pageXOffset ||
+            document.documentElement.scrollLeft,
+
+            // if any scroll is attempted,
+            // set this to the previous value
+                window.onscroll = function () {
+                window.scrollTo(scrollLeft, scrollTop);
+            };
+
+};
+function closepop(){
+    var popup = document.getElementById('content');
+    popup.style.display = "none";
+    var header = document.getElementsByTagName("header")[0];
+    header.style.display = "block";
+    document.getElementById("iframe").src = '';  
+    window.onscroll = function () { };
+};
+
+var prev='';
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction(id) {
+    var popup = document.getElementById(id);
+    if(prev==''){
+        popup.style.display = "block";
+        prev=id; 
+    }
+    else if (prev==id){
+        popup.style.display = "none";
+        prev=''
+    }
+    else {
+        document.getElementById(prev).style.display = "none";
+        popup.style.display = "block";
+        prev=id;
+    }
+    
+}
+    
